0bf9d4a3fafb34c7cecc3f6100e51e61499d389c
Kurteev Dmitry <kurteev.d@yandex.ru>
2022-04-03 19:47:26 +0300

    Catalog WIP

--- pages/catalog.js
+++ pages/catalog.js
@@ -1,138 +1,346 @@
-import React from "react";
-import Container from "@mui/material/Container";
+import { useRef, useEffect } from "react";
+import { Global, css } from "@emotion/react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import Image from "next/image";
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import CartIcon from "@mui/icons-material/ShoppingCart";
-import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
 import A from "@mui/material/Link";
-import Link from "next/link";
+import { SVG, Rect, Polygon, Gradient } from "@svgdotjs/svg.js";
+import { styled, useTheme } from "@mui/material/styles";
+import debounce from "lodash/debounce";
+import memoize from "lodash/memoize";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
-import lips from "../public/images/lips.jpg";
-import brows from "../public/images/brows.jpg";
-import face from "../public/images/face.jpg";
+const items = [
+  { id: "scrub", title: "scrub" },
+  { id: "oil", title: "dry oil" },
+  { id: "balm", title: "lip balm" },
+];
 
-const Catalog = () => {
+const Img = styled("img")``;
+
+export default function Catalog() {
   return (
     <>
-      <AppBar>
-        <Container>
-          <Toolbar disableGutters>
-            <Box sx={{ flexGrow: 1, display: "flex", alignItems: "center" }}>
-              <IconButton
-                size="large"
-                edge="start"
-                sx={{
-                  mr: 2,
-                  display: {
-                    sm: "none",
-                  },
-                }}
-              >
-                <MenuIcon />
-              </IconButton>
-              <Link href="/" passHref>
-                <A
-                  variant="h6"
-                  underline="none"
-                  color="text.primary"
-                  sx={{ mr: 6 }}
-                >
-                  LandB
-                </A>
-              </Link>
-              {["губы", "брови", "лицо"].map((title) => (
-                <Button
-                  key={title}
-                  size="large"
-                  sx={{
-                    display: {
-                      xs: "none",
-                      sm: "initial",
-                    },
-                  }}
-                >
-                  {title}
-                </Button>
-              ))}
-            </Box>
-            <IconButton size="large">
-              <CartIcon />
-            </IconButton>
-          </Toolbar>
-        </Container>
-      </AppBar>
-      <Container
+      <Global
+        styles={css`
+          html {
+            background: url(/images/catalog_background.jpg) no-repeat center
+              center fixed;
+            background-size: cover;
+          }
+          body {
+            background-color: transparent;
+          }
+        `}
+      />
+      <Box
         sx={{
-          mt: {
-            xs: 9,
-            sm: 12,
+          display: "flex",
+          width: {
+            sm: "100vw",
+          },
+          height: {
+            sm: "100vh",
+          },
+          alignItems: {
+            sm: "center",
+          },
+          pt: {
+            xs: 3,
+            sm: "initial",
           },
         }}
       >
-        {[
-          { id: "lips", title: "Губы", image: lips },
-          { id: "brows", title: "Брови", image: brows },
-          { id: "face", title: "Лицо", image: face },
-        ].map(({ id, title, image }) => (
-          <Box
-            key={id}
-            sx={{
-              display: "flex",
-              mb: 4,
-              gap: {
-                xs: 2,
-                sm: 4,
-              },
-              flexDirection: {
-                xs: "column",
-                sm: "row",
-              },
-            }}
-          >
-            <Box
-              sx={{
-                flexShrink: 0,
-                lineHeight: 0,
-                width: {
-                  xs: "100%",
-                  sm: "50%",
-                },
-              }}
-            >
-              <Image src={image} />
-            </Box>
-            <Box
-              sx={{
-                flexShrink: 1,
-                flexGrow: 1,
-              }}
-            >
-              <Typography variant="h3" gutterBottom>
-                {title}
-              </Typography>
-              <Typography variant="body1" paragraph>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Phasellus molestie elementum diam sed fermentum. Curabitur
-                efficitur lacus nec massa hendrerit ultrices. Proin at quam
-                odio. Cras in ipsum eget ligula scelerisque placerat a sit amet
-                est. In quis lorem tellus. Maecenas tincidunt libero at turpis
-                lacinia, et condimentum felis elementum. Sed id rutrum elit,
-                vitae feugiat nisi. Suspendisse eu vulputate sapien. Cras eget
-                diam tellus. Interdum et malesuada fames ac ante ipsum primis in
-                faucibus. Duis ornare elementum nibh, id gravida turpis gravida
-                ut.
-              </Typography>
-            </Box>
-          </Box>
-        ))}
-      </Container>
+        <Container
+          sx={{
+            display: "flex",
+            flexDirection: {
+              xs: "column",
+              sm: "row",
+            },
+            alignItems: {
+              xs: "center",
+              sm: "initial",
+            },
+            gap: {
+              xs: 5,
+              sm: 1,
+            },
+          }}
+        >
+          {items.map((item) => (
+            <CatalogItem key={item.id} {...item} />
+          ))}
+        </Container>
+      </Box>
     </>
   );
-};
+}
 
-export default Catalog;
+function CatalogItem({ id, title }) {
+  return (
+    <A
+      key={id}
+      href={`/catalog/${id}`}
+      underline="none"
+      sx={{
+        flexGrow: 1,
+        flexShrink: 1,
+        maxWidth: {
+          xs: 300,
+          sm: "initial",
+        },
+        width: {
+          xs: "100%",
+          sm: "30%",
+        },
+        display: "block",
+        position: "relative",
+        "& .circle, & .underline, & .title": {
+          opacity: 0.7,
+          transition: "opacity 0.2s ease-out",
+        },
+        "&:hover .circle, &:hover .underline, &:hover .title": {
+          opacity: 1,
+        },
+      }}
+    >
+      <Underline />
+      <Typography
+        className="title"
+        textAlign="center"
+        variant="h4"
+        sx={{
+          color: "common.white",
+          textShadow: "0 0 6px #fff",
+          mb: {
+            xs: 6,
+            md: 9,
+            lg: 12,
+          },
+        }}
+      >
+        {title}
+      </Typography>
+      <Box
+        sx={{
+          position: "relative",
+          padding: {
+            xs: 4,
+            md: 5,
+          },
+        }}
+      >
+        <Circle />
+        <Img
+          src={`/images/${id}.png`}
+          sx={{
+            pointerEvents: "none",
+            display: "block",
+            maxWidth: "100%",
+          }}
+        />
+      </Box>
+    </A>
+  );
+}
+
+const calculateCirclePoints = memoize(
+  (size, width = 8, offset = 0) => {
+    const pointsCount = 100;
+    const radius = size / 2 - offset;
+    const innerRadius = radius - width;
+    const pointAngle = 360 / pointsCount;
+
+    let outerPoints = [];
+    let innerPoints = [];
+
+    for (let i = 0, j = pointsCount; i <= pointsCount; i++, j--) {
+      const outerAngle = (pointAngle * i * Math.PI) / 180;
+      const outerX = radius * Math.sin(outerAngle) + radius + offset;
+      const outerY = radius * Math.cos(outerAngle) + radius + offset;
+
+      const innerAngle = (pointAngle * j * Math.PI) / 180;
+      const innerX = innerRadius * Math.sin(innerAngle) + radius + offset;
+      const innerY = innerRadius * Math.cos(outerAngle) + radius + offset;
+
+      outerPoints.push([outerX, outerY]);
+      innerPoints.push([innerX, innerY]);
+    }
+
+    return [...outerPoints, ...innerPoints];
+  },
+  (size, width, offset) => `${size}-${width}-${offset}`
+);
+
+function Circle() {
+  const theme = useTheme();
+  const isLarge = useMediaQuery(theme.breakpoints.up("md"));
+  const containerRef = useRef();
+
+  const circleWidth = isLarge ? 8 : 6;
+  const circleOffset = 19;
+  const glowWidth = circleWidth * 2;
+  const glowOffset = circleOffset - (glowWidth - circleWidth) / 2;
+  const blur = isLarge ? 9 : 5;
+
+  useEffect(() => {
+    if (!containerRef.current) return;
+
+    const canvas = SVG().addTo(containerRef.current).size("100%", "100%");
+
+    const gradient = new Gradient("linear");
+    gradient.stop(0, "#5688ff");
+    gradient.stop(1, "#ff00ff");
+    canvas.add(gradient);
+
+    const glow = new Polygon().fill(gradient).css("filter", `blur(${blur}px)`);
+    canvas.add(glow);
+
+    const circle = new Polygon().fill("#ffffff");
+    canvas.add(circle);
+
+    function draw() {
+      const size = containerRef.current.offsetWidth;
+      const circlePoints = calculateCirclePoints(
+        size,
+        circleWidth,
+        circleOffset
+      );
+      const glowPoints = calculateCirclePoints(size, glowWidth, glowOffset);
+
+      glow.plot(glowPoints);
+      circle.plot(circlePoints);
+    }
+
+    draw();
+
+    const debouncedDraw = debounce(draw, 500);
+    window.addEventListener("resize", debouncedDraw);
+
+    return () => {
+      canvas.remove();
+      window.removeEventListener("resize", debouncedDraw);
+    };
+  }, [
+    containerRef.current,
+    circleWidth,
+    circleOffset,
+    glowWidth,
+    glowOffset,
+    blur,
+  ]);
+
+  return (
+    <Box
+      className="circle"
+      ref={containerRef}
+      sx={{
+        position: "absolute",
+        left: 0,
+        top: 0,
+        width: "100%",
+        height: "100%",
+        pointerEvents: "none",
+      }}
+    />
+  );
+}
+
+function Underline() {
+  const containerRef = useRef();
+
+  useEffect(() => {
+    if (!containerRef.current) return;
+
+    const canvas = SVG().addTo(containerRef.current).size("100%", "100%");
+
+    const horizontalOffset = 40;
+    const rect1Offset = 11;
+    const rect2Offset = rect1Offset + 6;
+    const rect3Offset = rect2Offset + 1;
+    const rect4Offset = rect3Offset + 1;
+
+    const rect1Background = "#29d5f4";
+    const rect2Background = "#2ae9f4";
+    const rect3Background = "#ffffff";
+
+    const rect1 = new Rect();
+    canvas.add(rect1);
+
+    const rect2 = new Rect();
+    canvas.add(rect2);
+
+    const rect3 = new Rect();
+    canvas.add(rect3);
+
+    const rect4 = new Rect();
+    canvas.add(rect4);
+
+    function draw() {
+      const width = containerRef.current.offsetWidth;
+      const height = containerRef.current.offsetHeight;
+
+      rect1
+        .x(rect1Offset + horizontalOffset)
+        .y(rect1Offset)
+        .width(width - horizontalOffset * 2 - rect1Offset * 2)
+        .height(height - rect1Offset * 2)
+        .fill(rect1Background)
+        .radius((height - rect1Offset * 2) / 2)
+        .opacity(0.8)
+        .css("filter", "blur(3px)");
+
+      rect2
+        .x(rect2Offset + horizontalOffset)
+        .y(rect2Offset)
+        .width(width - horizontalOffset * 2 - rect2Offset * 2)
+        .height(height - rect2Offset * 2)
+        .fill(rect1Background)
+        .radius((height - rect2Offset * 2) / 2);
+
+      rect3
+        .x(rect3Offset + horizontalOffset)
+        .y(rect3Offset)
+        .width(width - horizontalOffset * 2 - rect3Offset * 2)
+        .height(height - rect3Offset * 2)
+        .fill(rect2Background)
+        .radius((height - rect3Offset * 2) / 2);
+
+      rect4
+        .x(rect4Offset + horizontalOffset)
+        .y(rect4Offset)
+        .width(width - horizontalOffset * 2 - rect4Offset * 2)
+        .height(height - rect4Offset * 2)
+        .fill(rect3Background)
+        .radius((height - rect4Offset * 2) / 2);
+    }
+
+    draw();
+
+    const debouncedDraw = debounce(draw, 500);
+    window.addEventListener("resize", debouncedDraw);
+
+    return () => {
+      canvas.remove();
+      window.removeEventListener("resize", debouncedDraw);
+    };
+  }, [containerRef.current]);
+
+  return (
+    <Box
+      className="underline"
+      ref={containerRef}
+      sx={{
+        position: "absolute",
+        top: {
+          xs: 35,
+          md: 45,
+        },
+        left: 0,
+        right: 0,
+        height: 40,
+      }}
+    />
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,137 @@
+import { useEffect, useState, useRef } from "react";
 import Head from "next/head";
 import { Global, css } from "@emotion/react";
+import { styled } from "@mui/material/styles";
+import debounce from "lodash/debounce";
 import Container from "@mui/material/Container";
-import Toolbar from "@mui/material/Toolbar";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 import Link from "next/link";
 import A from "@mui/material/Link";
 
+const Img = styled("img")``;
+
+const backgroundSize = {
+  width: 2300,
+  height: 2000,
+};
+const logoSize = {
+  width: 900,
+  height: 780,
+};
+const jingleSize = {
+  width: 756,
+  height: 161,
+};
+const logoPosition = {
+  left: (backgroundSize.width - logoSize.width) / 2,
+  top: 707,
+};
+
+const backgroundRatio = backgroundSize.width / backgroundSize.height;
+
+function getLogoRect(windowSize) {
+  const windowRatio = windowSize.width / windowSize.height;
+
+  if (windowRatio < backgroundRatio) {
+    const resize = windowSize.height / backgroundSize.height;
+
+    const resizedLogoSize = {
+      width: logoSize.width * resize,
+      height: logoSize.height * resize,
+    };
+    const resizedBackgroundSize = {
+      width: backgroundSize.width * resize,
+      height: backgroundSize.height * resize,
+    };
+    const resizedLogoPosition = {
+      left:
+        logoPosition.left * resize -
+        (resizedBackgroundSize.width - windowSize.width) / 2,
+      top: logoPosition.top * resize,
+    };
+
+    return { ...resizedLogoSize, ...resizedLogoPosition };
+  } else {
+    const resize = windowSize.width / backgroundSize.width;
+
+    const resizedLogoSize = {
+      width: logoSize.width * resize,
+      height: logoSize.height * resize,
+    };
+    const resizedBackgroundSize = {
+      width: backgroundSize.width * resize,
+      height: backgroundSize.height * resize,
+    };
+    const resizedLogoPosition = {
+      left: logoPosition.left * resize,
+      top:
+        logoPosition.top * resize -
+        (resizedBackgroundSize.height - windowSize.height) / 2,
+    };
+
+    if (resizedLogoPosition.top + resizedLogoSize.height > windowSize.height) {
+      resizedLogoSize.height = windowSize.height - resizedLogoPosition.top;
+    }
+
+    return { ...resizedLogoSize, ...resizedLogoPosition };
+  }
+}
+
+function getJingleRect(windowSize, logoRect) {
+  const width = Math.min(jingleSize.width, logoRect.width);
+  const ratio = width / jingleSize.width;
+  const height = jingleSize.height * ratio;
+  const top = (logoRect.top - height) / 2;
+  const left = (windowSize.width - width) / 2;
+
+  return { width, height, top, left };
+}
+
 export default function Home() {
+  const logoRef = useRef(null);
+  const jingleRef = useRef(null);
+
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    function draw() {
+      const windowSize = {
+        width: document.documentElement.clientWidth,
+        height: document.documentElement.clientHeight,
+      };
+
+      const logoRect = getLogoRect(windowSize);
+      const jingleRect = getJingleRect(windowSize, logoRect);
+
+      logoRef.current.style.width = `${logoRect.width}px`;
+      logoRef.current.style.height = `${logoRect.height}px`;
+      logoRef.current.style.left = `${logoRect.left}px`;
+      logoRef.current.style.top = `${logoRect.top}px`;
+
+      jingleRef.current.style.width = `${jingleRect.width}px`;
+      jingleRef.current.style.height = `${jingleRect.height}px`;
+      jingleRef.current.style.left = `${jingleRect.left}px`;
+      jingleRef.current.style.top = `${jingleRect.top}px`;
+
+      setVisible(true);
+    }
+
+    const redraw = debounce(draw, 500);
+
+    draw();
+
+    window.addEventListener("resize", redraw);
+
+    return () => window.removeEventListener("resize", redraw);
+  }, [setVisible]);
+
   return (
     <>
       <Global
         styles={css`
           html {
-            background: url(/images/background.jpg) no-repeat center center
+            background: url(/images/home_background.jpg) no-repeat center center
               fixed;
             background-size: cover;
           }
@@ -27,16 +147,55 @@ export default function Home() {
       </Head>
       <Link href="/catalog" passHref>
         <A
+          ref={logoRef}
+          underline="none"
           sx={{
-            display: "block",
-            height: "100vh",
-            width: "100vw",
-            textDecoration: "none",
+            position: "absolute",
+            width: 0,
+            height: 0,
+            x: 0,
+            y: 0,
           }}
         >
           &nbsp;
         </A>
       </Link>
+      <Link href="/catalog" passHref>
+        <A
+          ref={jingleRef}
+          underline="none"
+          sx={{
+            position: "absolute",
+            width: 0,
+            height: 0,
+            x: 0,
+            y: 0,
+          }}
+        >
+          <Img
+            src="/images/jingle.png"
+            sx={{
+              transition: "opacity 0.5s ease-out, transform 0.5s ease-out",
+              opacity: visible ? 1 : 0,
+              transform: visible ? "translateY(0)" : "translateY(-10px)",
+              maxWidth: "100%",
+            }}
+          />
+        </A>
+      </Link>
     </>
   );
 }
+
+// <Typography
+//   variant="h4"
+//   sx={{
+//     textTransform: "uppercase",
+//     color: "text.primary",
+//     fontWeight: "bold",
+//     letterSpacing: 3,
+//     animation: "neonAnimation 1s infinite",
+//   }}
+// >
+//   посмотреть
+// </Typography>
